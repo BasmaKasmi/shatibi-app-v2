d99deda22696380c899de7c436bac43a267bd973
Basma kasmi <dev@shatibi.fr>
2025-08-25 14:37:59 +0200

    fix(models): replace EvalSheet export with try/catch to fix build error

--- models/evalSheet.ts
+++ models/evalSheet.ts
@@ -1,21 +1,29 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const schema = mongoose.Schema;
 const objId = schema.ObjectId;
 
 const evalSheetSchema = new schema({
-    modId: {
-        type: objId,
-        ref: "EvalMod", // Use model name directly to avoid circular dependency
-        required: true,
-    },
-    subject: { type: String, required: true },
-    passageDate: { type: Date, required: true },
-    sendDate: { type: Date, required: false },
-    statut: { type: String, required: true, default: "À venir" },
-    scaleId: { type: objId, ref: "Scale", required: true },
-    groupId: { type: String, required: true },
-    delete: { type: Boolean, required: false}
+  modId: {
+    type: objId,
+    ref: "EvalMod", // Use model name directly to avoid circular dependency
+    required: true,
+  },
+  subject: { type: String, required: true },
+  passageDate: { type: Date, required: true },
+  sendDate: { type: Date, required: false },
+  statut: { type: String, required: true, default: "À venir" },
+  scaleId: { type: objId, ref: "Scale", required: true },
+  groupId: { type: String, required: true },
+  delete: { type: Boolean, required: false },
 });
 
-export default mongoose.models.EvalSheet || mongoose.model('EvalSheet', evalSheetSchema);
+let EvalSheetModel;
+
+try {
+  EvalSheetModel = mongoose.model("EvalSheet");
+} catch {
+  EvalSheetModel = mongoose.model("EvalSheet", evalSheetSchema);
+}
+
+export default EvalSheetModel;
