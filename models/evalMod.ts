22e26389462a50b4d05f2e97ad004545b008b900
Basma kasmi <dev@shatibi.fr>
2025-08-25 14:20:05 +0200

    Fix TypeScript error in evalMod model

--- models/evalMod.ts
+++ models/evalMod.ts
@@ -1,17 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const schema = mongoose.Schema;
-const objId = schema.ObjectId;
+const { Schema } = mongoose;
+const { ObjectId } = Schema.Types;
 
-const evalModSchema = new mongoose.Schema({
-    title: { type: String, required: true, unique: true },
-    description: { type: String },
-    format: { type: String, required: true },
-    scale: { type: [objId], ref: 'Scale', required: true },
-    coef: { type: Number, required: true },
-    teacherId: { type: [String] },
-    department: { type: [String], required: true },
-    delete: { type: Boolean, required: false}
+const evalModSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String },
+  format: { type: String, required: true },
+  scale: { type: [ObjectId], ref: "Scale", required: true },
+  coef: { type: Number, required: true },
+  teacherId: { type: [String] },
+  department: { type: [String], required: true },
+  delete: { type: Boolean, required: false },
 });
 
-export default mongoose.models.EvalMod || mongoose.model('EvalMod', evalModSchema);
+const EvalMod =
+  mongoose.models.EvalMod || mongoose.model("EvalMod", evalModSchema);
+
+export default EvalMod;
